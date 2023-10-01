b4352e20f869c360459e6a6564f4cebd600f1927
James Orr <2550508o@student.gla.ac.uk>
2023-10-01 19:05:24 +0100

    Docs and code updates

--- reflective_note/main.js
+++ reflective_note/main.js
@@ -3,21 +3,29 @@ define([
     'base/js/events'
     ], function(Jupyter, events) {
 
-      var insert_cell = function() {
+      var insert_info_cell = function() {
         Jupyter.notebook.
-        insert_cell_below('code').
-        set_text(`# Make a note on the previous task!`);
-        Jupyter.notebook.select_prev();
-        //Jupyter.notebook.execute_cell_and_select_below();
+        insert_cell_below('markdown').
+        set_text("Make a note on the previous task! Think about what you found difficult and what went well. Reflect on something");
+        Jupyter.notebook.select_next();
+        Jupyter.notebook.execute_cell();
+        insert_response_cell();
+      };
+
+      var insert_response_cell = function() {
+        Jupyter.notebook.
+        insert_cell_below('markdown').
+        set_text("Type you reflective note here!");
+        Jupyter.notebook.select_next();
       };
       // Add Toolbar button
       var planetJupyterButton = function () {
           console.log();
           Jupyter.toolbar.add_buttons_group([
               Jupyter.keyboard_manager.actions.register ({
-                  'help': 'Add note for self reflection',
+                  'help': 'Add note for self reflection xd',
                   'icon' : 'fa-paper-plane',
-                  'handler': insert_cell
+                  'handler': insert_info_cell
               }, 'addplanetjupyter-cell', 'Reflective Note')
           ])
       }
@@ -25,7 +33,7 @@ define([
     function load_ipython_extension() {
         // Add a default cell if there are no cells
         if (Jupyter.notebook.get_cells().length===1){
-            insert_cell();
+            insert_info_cell();
         }
         planetJupyterButton();
     }
